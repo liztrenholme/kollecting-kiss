ad67b88b0e3a379478a5e3be5ee0e0108af537aa
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2018-12-23 23:56:39 -0500

    separated login form into new component, integrated with admin form

--- src/components/adminLogin.js
+++ src/components/adminLogin.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-const {
-  Stitch,
-  RemoteMongoClient,
-  AnonymousCredential
-} = require('mongodb-stitch-browser-sdk');
+import Admin from './admin';
+import Form from './loginForm';
+
+const { Stitch } = require('mongodb-stitch-browser-sdk');
 
 const client = Stitch.defaultAppClient;
 
@@ -15,43 +14,60 @@ class adminLogin extends Component {
     this.state = {
       username: '',
       password: '',
-      message: ''
+      message: '',
+      authed: false,
+      whichView: this.showWhichView(this.authed)
     };
-  this.handleChange = this.handleChange.bind(this);
-  this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.showWhichView = this.showWhichView.bind(this);
   }
 
 
   handleChange(event) {
     this.setState({
-      [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
+      message: ''
     })
   }
 
   handleSubmit = (e) => {
     e.preventDefault();
     client.callFunction("login", [this.state.username, this.state.password]).then(result => {
-      console.log(result) // Output: 7
-  });
+      console.log(result);
+      if (result === true) {
+        this.setState({
+          authed: true
+        })
+        // return <Admin />
+      }
+      else if (result === false || result === null || result === undefined) {
+        this.setState({
+          username: '',
+          password: '',
+          message: "Incorrect Username and/or Password",
+          authed: false
+        })
+      }
+    })
   }
-  showMe = (items) => {
-    console.log(items);
-  }
+
+    showWhichView(authed) {
+      if (authed === false) {
+        return <Form />
+      }
+      else if (authed === true) {
+        return <Admin />
+      }
+    }
+  
 
   render() {
     return (
-      <div className="container">
-        <form className="authform" onSubmit={this.handleSubmit}>
-          <h2 className="form-signin-heading">Please sign in</h2>
-          <label htmlFor="inputName" >Email address</label>
-          <input type="text" className="form-control" placeholder="Username" name="username" value={this.state.username} onChange={this.handleChange} required />
-          <label htmlFor="inputPassword" className="sr-only">Password</label>
-          <input type="password" className="form-control" placeholder="Password" name="password" value={this.state.password} onChange={this.handleChange} required />
-          <button className="btn btn-outline-secondary" type="submit">Login</button>
-          <p>
-            Not a member? <Link to="/admin"><span className="glyphicon glyphicon-plus-sign" aria-hidden="true"></span> Register here</Link>
-          </p>
-        </form>
+      <div>
+        {this.state.whichView}
+        {console.log(this.state.authed)}
+        <Form />
       </div>
     );
   }
---
+++ src/components/loginForm.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './mem.css';
+
+class Form extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+    render() {
+        return (
+            <div className="Login">
+                <div className="row">
+                    <div className="col-md-1">
+                    </div>
+                    <div className="col-md-10 authform">
+                        <form onSubmit={this.handleSubmit}>
+                            <h2>Please sign in</h2>
+                            <input type="text" className="text-input" placeholder="Username" name="username" value={this.props.username} onChange={this.handleChange} required />
+                            <input type="password" className="text-input" placeholder="Password" name="password" value={this.props.password} onChange={this.handleChange} required />
+                            <button className="btn" type="submit">Login</button>
+                        </form>
+                        <p className="error-msg" style={{ color: 'red' }}>{this.props.message}</p>
+                        <div className="col-md-1">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Form;
