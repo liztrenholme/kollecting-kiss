c137d7bf127eb5efa50a80c9e172765ed81fab78
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2018-12-24 19:45:12 -0500

    Updates

---
+++ precache-manifest.e090cc37984598e57c6f548a757dbead.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d0c6e9b615cf20337f8",
+    "url": "/kollecting-kiss/static/css/main.15fd1f4a.chunk.css"
+  },
+  {
+    "revision": "6d0c6e9b615cf20337f8",
+    "url": "/kollecting-kiss/static/js/main.6d0c6e9b.chunk.js"
+  },
+  {
+    "revision": "e60188e9db0b56803285",
+    "url": "/kollecting-kiss/static/js/1.e60188e9.chunk.js"
+  },
+  {
+    "revision": "b605d26b6f5ffa88567d",
+    "url": "/kollecting-kiss/static/js/runtime~main.b605d26b.js"
+  },
+  {
+    "revision": "b741b66d8eead407f1fbe79fc4ff680a",
+    "url": "/kollecting-kiss/static/media/logo.b741b66d.jpg"
+  },
+  {
+    "revision": "bc6b8447c19eb1ea1b28cf37ae2d0c0e",
+    "url": "/kollecting-kiss/static/media/header.bc6b8447.jpg"
+  },
+  {
+    "revision": "e0e054d7a586ae0d7b85ab7f17823a51",
+    "url": "/kollecting-kiss/index.html"
+  }
+];
