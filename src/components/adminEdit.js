b5f8d5263fae698fb8716c4805bacacdf998236d
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2020-01-23 22:16:45 -0500

    made into dumb component to recieve props

--- src/components/adminEdit.js
+++ src/components/adminEdit.js
@@ -2,57 +2,35 @@ import React, { Component } from 'react';
 // import { NavLink } from 'react-router-dom';
 import './mem.css';
 import PropTypes from 'prop-types'; 
-import stitchClient from './stitch';
-
-const {
-  RemoteMongoClient,
-  AnonymousCredential
-} = require('mongodb-stitch-browser-sdk');
-  
 
 class AdminEdit extends Component {
-state = {
-  items: {}
-}
-
-componentDidMount() {
-  stitchClient.auth.loginWithCredential(new AnonymousCredential()).then(() => {
-    const mongodb = stitchClient.getServiceClient(
-      RemoteMongoClient.factory,
-      'mongodb-atlas'
-    );
-    const items = mongodb.db('memorabilia').collection('items');
-    return items.find({}).asArray();
-  })
-    .then(items => this.setState({ items: items }));
-}
-render() {
-  const { items } = this.state;
-  const itemsArr = Object.keys(items).map(i => items[i]);
-  return (
-    <div style={{backgroundColor: '#fff'}}>
-      <div className="row">
-        <div className="col-md-12">
-          <div className="grid-container">
-            {itemsArr.map(item =>
-              <div className="grid-item"
-                grn={item.grn}
-                key={item._id} 
-                data-key={item._id}
-                data-valuename={item.itemName} 
-                onClick={this.props.openModal(item)}>
-                <h4 className="listing-title">{item.itemName}</h4> 
-                <p className="listing-description">{item.description}</p>
-                <img className="featured-image" src={item.mainImage || item.imageURL[0] || ''} 
-                  alt={item.itemName} height="100px" />
-              </div>
-            )}
+  render() {
+    const { items } = this.props;
+    const itemsArr = Object.keys(items).map(i => items[i]);
+    return (
+      <div style={{backgroundColor: '#fff'}}>
+        <div className="row">
+          <div className="col-md-12">
+            <div className="grid-container">
+              {itemsArr.map(item =>
+                <div className="grid-item"
+                  grn={item.grn}
+                  key={item._id} 
+                  data-key={item._id}
+                  data-valuename={item.itemName} 
+                  onClick={this.props.openModal(item)}>
+                  <h4 className="listing-title">{item.itemName}</h4> 
+                  <p className="listing-description">{item.description}</p>
+                  <img className="featured-image" src={item.mainImage || item.imageURL[0] || ''} 
+                    alt={item.itemName} height="100px" />
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 }
 
 AdminEdit.propTypes = {
