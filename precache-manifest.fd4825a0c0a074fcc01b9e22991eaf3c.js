6231258658561f3216471f1084b605ad487ca6a2
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2020-03-06 22:08:27 -0500

    Updates

---
+++ precache-manifest.fd4825a0c0a074fcc01b9e22991eaf3c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7771b30c76871abd7aeb",
+    "url": "/static/css/main.c438996b.chunk.css"
+  },
+  {
+    "revision": "7771b30c76871abd7aeb",
+    "url": "/static/js/main.7771b30c.chunk.js"
+  },
+  {
+    "revision": "419072342d34447850ee",
+    "url": "/static/css/1.afa6a00f.chunk.css"
+  },
+  {
+    "revision": "419072342d34447850ee",
+    "url": "/static/js/1.41907234.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0716cc9126d276141975ce1faf40ae79",
+    "url": "/static/media/kissbanner.0716cc91.jpg"
+  },
+  {
+    "revision": "b741b66d8eead407f1fbe79fc4ff680a",
+    "url": "/static/media/logo.b741b66d.jpg"
+  },
+  {
+    "revision": "eac60a0d198d4b10953ad38231efb7e8",
+    "url": "/index.html"
+  }
+];
