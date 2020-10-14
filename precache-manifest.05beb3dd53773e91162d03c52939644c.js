3885a1f84b035ac721cf8b85e6c9bc93c8144251
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2020-10-13 21:59:29 -0400

    Updates

---
+++ precache-manifest.05beb3dd53773e91162d03c52939644c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "add2143384704aab9c7b3f69d04be17f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "100fbc4667cd33161e6f",
+    "url": "/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "c65c1658a6d9c0132685",
+    "url": "/static/css/main.9ef662a1.chunk.css"
+  },
+  {
+    "revision": "100fbc4667cd33161e6f",
+    "url": "/static/js/2.cff16b65.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/static/js/2.cff16b65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c65c1658a6d9c0132685",
+    "url": "/static/js/main.a0197a60.chunk.js"
+  },
+  {
+    "revision": "4cffe002422264e4b48b",
+    "url": "/static/js/runtime-main.47bd2d76.js"
+  },
+  {
+    "revision": "2b1c307939ecc0654d1b0afad95cb0f9",
+    "url": "/static/media/KISS-SHOPTOUR-BANNER_2_1800x.2b1c3079.jpg"
+  },
+  {
+    "revision": "16e06eb58c2fd91b0bc79ee4b3ff7ee1",
+    "url": "/static/media/Kulick_Merch_Banner.16e06eb5.jpg"
+  },
+  {
+    "revision": "bacda51724cdd86be3311a4d72d55299",
+    "url": "/static/media/UnholyRadio.bacda517.jpg"
+  },
+  {
+    "revision": "dd578c41189d3e760a9ee2079ee50b32",
+    "url": "/static/media/instagram.dd578c41.png"
+  },
+  {
+    "revision": "0716cc9126d276141975ce1faf40ae79",
+    "url": "/static/media/kissbanner.0716cc91.jpg"
+  },
+  {
+    "revision": "b741b66d8eead407f1fbe79fc4ff680a",
+    "url": "/static/media/logo.b741b66d.jpg"
+  },
+  {
+    "revision": "8ec1ee0a4f3ddbc50d1a0381d996995a",
+    "url": "/static/media/museum.8ec1ee0a.jpg"
+  }
+]);
